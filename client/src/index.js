24c07dfc457c706b5891432092ab161ac2866dd7
Tobi van Bronswijk <t.vanbronswijk@student.fontys.nl>
2018-06-11 12:34:01 +0200

    added client

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+
+const router = (
+    <div />
+);
+
+ReactDOM.render(router, document.getElementById('root'));
+registerServiceWorker();
